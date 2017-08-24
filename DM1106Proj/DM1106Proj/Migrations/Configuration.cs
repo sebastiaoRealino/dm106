7ea61bb0699456c85dcfde1c24a6f6d24ae0acd8
sebastiaoRealino <sebastiaorealino@yahoo.com.br>
2017-08-24 01:25:08 -0300

    Adicionando migrações para os modelos adicionados anteriormente

---
+++ DM1106Proj/DM1106Proj/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace DM1106Proj.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DM1106Proj.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DM1106Proj.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
