b35cc6fb8c4c99e348c6fc5ac3bf234f89b17d28
sebastiaoRealino <sebastiaorealino@yahoo.com.br>
2017-08-24 01:18:21 -0300

    Criação do serviço de Pedidos juntamente com item de pedidos

---
+++ DM1106Proj/DM1106Proj/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DM1106Proj.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            this.OrderItems = new HashSet<OrderItem>();
+        }
+        public int Id { get; set; }
+        public string userEmail { get; set; }
+        public DateTime deliveryDate { get; set; }
+        public DateTime creationDate { get; set; }
+        public string status { get; set; }
+
+        public decimal totalPrice { get; set; }
+        public decimal freightRate { get; set; }
+        public float totalWeight { get; set; }
+        public virtual ICollection<OrderItem> OrderItems
+        {
+            get;
+            set;
+        }
+    }
+}
